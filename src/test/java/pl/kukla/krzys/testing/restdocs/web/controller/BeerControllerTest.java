9922da6d07ae59955c95eb1d6592279f3311e613
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-01-18 18:42:54 +0100

    200 Rest-docs SpringMvc configuration

--- src/test/java/pl/kukla/krzys/testing/restdocs/web/controller/BeerControllerTest.java
+++ src/test/java/pl/kukla/krzys/testing/restdocs/web/controller/BeerControllerTest.java
@@ -2,12 +2,15 @@ package pl.kukla.krzys.testing.restdocs.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import pl.kukla.krzys.testing.restdocs.domain.Beer;
@@ -27,6 +30,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 /**
  * @author Krzysztof Kukla
  */
+//autoonfiguration REST docs for us
+@ExtendWith(value = RestDocumentationExtension.class) //we need to add SpringExtension as well, but this is done int @WebMvcTest
+@AutoConfigureRestDocs
 @WebMvcTest(controllers = BeerController.class)
 @ComponentScan(value = "pl.kukla.krzys.testing.restdocs.web.mapper")
 class BeerControllerTest {
